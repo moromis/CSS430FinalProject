c4845dcdeb865f1152a571e70459009e42fb71a1
pm_Vision <pmar116@uw.edu>
2018-03-04 01:05:13 -0800

    implement close, fsize

--- Classes/FileSystem.java
+++ Classes/FileSystem.java
@@ -31,8 +31,9 @@ public class FileSystem {
 	
 	void sync() {
 
-		
+		//TODO: implement
 	}
+	
 	/*
 	*	Format the max number of inodes
 	*	file number = # inodes
@@ -47,30 +48,56 @@ public class FileSystem {
 		
 	}
 	
-
 	FileTableEntry open( String filename, String mode ) {
 		
 		FileTableEntry ftEnt = filetable.falloc( filename, mode );
 		if( mode.equals( "w" ) {
-			if( deallocAllBlocks( ftEnt ) == false ) //TODO: implement deallocAllBlocks
+			if( deallocAllBlocks( ftEnt ) == false )
 				return null;
 		}
 		return ftEnt;
 	}
 	
-	
+	/*
+	*	Closes the file ftEnt
+	*	commits all file transacitons on the file
+	*	removes from user file descriptior table of calling thread TCB
+	*
+	*	returns true in success
+	*/
 	boolean close( FileTableEntry ftEnt ) {
-		
-		//TODO: implement
-	
+		//be th eonly one touching the file
+		synchronized(ftEnt){
+			//decrement # of users
+			ftEnt.count--;
+			if(ftEnt.count > 0){
+				//return true if other people are using the file
+				return true;
+			}
+		}
+		//if you are the last one to use the file save everything
+		return filetable.ffree(ftEnt);
 	}
 	
+	/*
+	*	Reads up to buffer.length bytes from the file starting at the seek pointer
+	*	returns the number of bytes read in
+	*	returns -1 in fail
+	*/
 	int read( FileTableEntry ftEnt, byte[] buffer ) {
-		
-		//TODO: implement
-		
+		//make sure its the right mode
+		if(ftEnt.mode == "r" || ftEnt.mode == "w+"){
+			int readin = 0;
+			int buflen = buffer.length;
+
+			synchronized(ftEnt){
+				while(buflen > 0 && )
+			}
+		}else{
+			//bad input
+			return -1;
+		}		
 	}
-	
 	
 	int write( FileTableEntry ftEnt, byte[] buffer ) {
 		
@@ -78,10 +105,13 @@ public class FileSystem {
 		
 	}
 	
-	
+	/*
+	*	resturns the size in bytes of the file indicated by ftEnt
+	*/
 	int fsize( FileTableEntry ftEnt ) {
-		
-		//TODO: implement
+		synchronized(ftEnt){
+			return ftEnt.inode.length;
+		}
 		
 	}
 	
@@ -95,8 +125,10 @@ public class FileSystem {
 	*	get entry and remove from directory
 	*/
 	boolean delete( String filename ) {
+		//open to dealloc blocks - this updates the inode
 		FileTableEntry entry = this.open(filename, "w");
 		short info = entry.iNumber;
+		//close the entry and return if it closes or not
 		return this.close(entry) && this.directory.ifree(info);
 		
 	}
@@ -104,7 +136,6 @@ public class FileSystem {
 	private final int SEEK_SET = 0;
 	private final int SEEK_CUR = 1;
 	private final int SEEK_END = 2;
-	
 	
 	int seek( FileTableEntry ftEnt, int offset, int whence ) {
 		
