bb3328760e9f029fd61af8e818695c48c46cc5fd
daniel-grimm <odysseyporthole@gmail.com>
2018-03-08 00:19:49 -0800

    Added comments and fixed the file so it compiles.

--- Classes/Directory.java
+++ Classes/Directory.java
@@ -13,11 +13,11 @@ public class Directory {
    // Directory entries
    private int fsize[];        // each element stores a different file size.
    private char fnames[][];    // each element stores a different file name.
-   private fileCounter = 0;
+   private int fileCounter = 0;
 
    public Directory( int maxInumber ) { // directory constructor
    
-      fsizes = new int[maxInumber];     // maxInumber = max files
+      fsize = new int[maxInumber];     // maxInumber = max files
 	  
       for ( int i = 0; i < maxInumber; i++ ) 
          fsize[i] = 0;                 // all file size initialized to 0
@@ -25,15 +25,16 @@ public class Directory {
       fnames = new char[maxInumber][maxChars];
       String root = "/";                // entry(inode) 0 is "/"
       fsize[0] = root.length( );        // fsize[0] is the size of "/".
-      root.getChars( 0, fsizes[0], fnames[0], 0 ); // fnames[0] includes "/"
+      root.getChars( 0, fsize[0], fnames[0], 0 ); // fnames[0] includes "/"
 	  
 	  fileCounter++;
    }
 
-   /** This method
-   @param 
-   @return*/
-   public void bytes2directory( byte data[] ) {
+   /** This method reads in a byte array of data from the disk
+   and changes it into a directory.
+   @param data : The directory from the disk.
+   @return void*/
+   public void bytes2directory( byte[] data ) {
       // assumes data[] received directory information from disk
       // initializes the Directory instance with this data[]
 	  
@@ -41,9 +42,9 @@ public class Directory {
 	  //read in fsize, fnames, and fileCounter
    }
 
-   /** This method
-   @param 
-   @return*/
+   /** This method writes the directory information into a byte array to be
+   stored on the disk.
+   @return byte[] : The directory as stored in a byte array.*/
    public byte[] directory2bytes( ) {
       // converts and return Directory information into a plain byte array
       // this byte array will be written back to disk
@@ -53,27 +54,29 @@ public class Directory {
 	  //called by filesystem - likely in sync
 	  
 	  //use SysLib.int2bytes and store fsize, fnames, and fileCounter in a byte array, and then return that array
+      return new byte[0];
    }
 
-   /** This method
-   @param 
-   @return*/
+   /** This method allocates an inode for the new file.
+   @param filename : The name of the new file.
+   @return short : The address of the inode.*/
    public short ialloc( String filename ) {
 	   
       // filename is the one of a file to be created.
       // allocates a new inode number for this filename
 	  
 	  fsize[fileCounter] = filename.length();
-	  filename.getChars( 0, fsizes[fileCounter], fnames[fileCounter], 0 );
+	  filename.getChars( 0, fsize[fileCounter], fnames[fileCounter], 0 );
 	  
 	  //TODO: allocate an inode number? how to do?
 	  //from slides: create new inode: check if there's a free inode and assign it to the file,
 	  //return inode number, otherwise return error (-1)
+     return -1;
    }
 
-   /** This method
-   @param 
-   @return*/
+   /** This method frees up an inode using the supplied inode number.
+   @param iNumber : The inode number to be freed up.
+   @return boolean : False if the operation was not successful, otherwise true.*/
    public boolean ifree( short iNumber ) {
 	   
       // deallocates this inumber (inode number)
@@ -83,14 +86,16 @@ public class Directory {
 	  
 	  String thefilename = "";	//TODO get the file name
 	  SysLib.delete(thefilename);
+     return false;
    }
 
-   /** This method
-   @param 
-   @return*/
+   /** This method returns the inode number that corresponds with the file name.
+   @param filename : The name of the file.
+   @return short : The inode number.*/
    public short namei( String filename ) {
       // returns the inumber corresponding to this filename
 	  
 	  //return -1 if no inode is associated with the filename - can happen
+      return -1;
    }
 }
